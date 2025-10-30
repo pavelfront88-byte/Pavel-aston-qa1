eb90e07d0a637e0c15c2fe058c3938a37389680f
Pavel <pavelfront88@>
2025-10-30 12:46:04 +0500

    New project

---
+++ .idea/Home dz2.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
