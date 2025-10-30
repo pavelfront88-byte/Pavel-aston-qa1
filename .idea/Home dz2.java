f0c5323b7dab6b4939e530afd32c7b6db4db82da
pavelfront88-byte <pavelfront88@gmail.com>
2025-10-30 13:57:39 +0500

    Delete .idea/Home dz2.java

--- .idea/Home dz2.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void printThreeWords() {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
-    }
